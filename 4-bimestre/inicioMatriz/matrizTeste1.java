8fe4c5b33aa1fd04fbc9e321ed3ceabcb0beb016
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2023-11-16 20:14:38 -0300

    Create matrizTeste1.java

---
+++ 4-bimestre/inicioMatriz/matrizTeste1.java
@@ -0,0 +1,20 @@
+package matrix_1;
+import java.util.Random;
+public class matrizTeste1 {
+	public static void main(String[] args) {
+		int v [][] = new int [3][3];
+		int i, j;
+		Random in = new Random();
+		for(i=0; i<3;i++) {
+			for(j=0; j<3;j++) {
+				v[i][j]= in.nextInt()%50;
+			}	
+		}
+		for(i=0;i<3;i++) {
+			System.out.println();
+			for(j=0;j<3;j++) {
+				System.out.print("[ "+v[i][j]+"]");
+			}
+		}
+	}
+}
