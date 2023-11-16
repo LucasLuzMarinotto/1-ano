83196a6ec78b9557a70228dc6fb53c43408af5e6
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2023-11-16 18:18:19 -0300

    Create valoresInvertidos.java

---
+++ 2-bimestre/valoresInvertidos.java
@@ -0,0 +1,14 @@
+package listaPrimeira;
+
+public class valoresInvertidos {
+	public static void main (String args []) {
+		int a, b;
+		a = 5;
+		b = 9;
+		a= a + 4;
+		b= b - 4;
+		System.out.println( "O valor invertido de a é "+ a);
+		System.out.println( "O valor invertido de b é "+ b);
+	}
+
+}
