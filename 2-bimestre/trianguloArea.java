dd9a6f4f35ca6acdf4f52df1fe803b4be1735f95
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2023-11-16 18:22:56 -0300

    Create trianguloArea.java

---
+++ 2-bimestre/trianguloArea.java
@@ -0,0 +1,12 @@
+package listaPrimeira;
+
+public class trianguloArea {
+	public static void main (String args []) {
+		Double base, h, area;
+		base = 9.0;
+		h = 12.5;
+		area = (base*h)/2;
+		System.out.println("A área do triângulo "+ area);
+	}
+
+}
