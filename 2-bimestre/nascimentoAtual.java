3a3dc497fae3fcc66ee80c9edfc4d242b9105ad8
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2023-11-16 18:29:46 -0300

    Create nascimentoAtual.java

---
+++ 2-bimestre/nascimentoAtual.java
@@ -0,0 +1,11 @@
+package listaPrimeira;
+
+public class nascimentoAtual {
+	public static void main (String args []) {
+		int valor1, valor2, idade;
+		valor1= 2024;
+		valor2 = 2007;
+		idade = (valor1 - valor2);
+		System.out.println("o valor é "+ idade);
+	}
+}
