2272421e6f3e9bcbd1bc9abd3f3e99782b65d31c
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2023-11-16 20:00:56 -0300

    Create programaMultiplo.java

---
+++ 3-bimestre/listaDoWhile/programaMultiplo.java
@@ -0,0 +1,13 @@
+package listaDowhile;
+
+public class programaMultiplo {
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=100; i++) {
+			System.out.println(i);
+			if(i % 10 ==0){
+				System.out.println(i+ " Múltiplo de 10");
+			}
+		}
+	}
+}
