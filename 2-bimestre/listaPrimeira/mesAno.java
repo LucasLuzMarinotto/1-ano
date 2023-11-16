0733d9d0c1d7ef95085c282d22c9b2d91ff6976d
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2023-11-16 18:33:44 -0300

    Create mesAno.java

---
+++ 2-bimestre/listaPrimeira/mesAno.java
@@ -0,0 +1,13 @@
+package listaPrimeira;
+
+public class mesAno {
+	public static void main (String args []) {
+		double dia, mes, ano;
+		dia = 1.200;
+		mes = dia/30;
+		ano = dia/365;
+		System.out.println ("Os dias convertidos para meses "+ mes);
+		System.out.println ("Os dias convertidos para anos são "+ ano);
+	}
+
+}
