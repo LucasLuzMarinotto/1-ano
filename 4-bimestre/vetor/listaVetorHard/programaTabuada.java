216d3e500f689b671af3b30640d492c99c1f1055
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2023-11-17 08:17:53 -0300

    Update programaTabuada.java

--- 4-bimestre/vetor/listaVetorHard/programaTabuada.java
+++ 4-bimestre/vetor/listaVetorHard/programaTabuada.java
@@ -1,1 +1,24 @@
+package hard;
+import java.util.Scanner;
+public class Programatabuada {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		final int TAM=5;
+		int a[], i, mult, j;
+		a = new int[TAM];
+		
+		
+		for(i=0; i<TAM; i++) {
+			System.out.println("digite o valor "+(i+1)+"º de A:");
+			a[i] = ler.nextInt();
+			
+			for(j=0; j<=10; j++) {
+				mult = a[i]*j;
+				System.out.println(a[i] +" x "+j + " = "+mult);
+			}
+			
+		}
+		
+	}
 
+}
