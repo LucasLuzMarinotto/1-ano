62d2c71c2a41984e4ef126396f19a679606a9efd
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2023-11-23 20:00:19 -0300

    Create programaFatorial.java

---
+++ 4-bimestre/vetor/listaVetorHard/programaFatorial.java
@@ -0,0 +1,38 @@
+package hard;
+import java.util.Scanner;
+public class Programafatorial {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int a[], b[];
+        
+        a = new int[3];
+        b = new int[3];
+        int i, j, num;
+        int fat = 1;
+
+        for (i = 0; i < 3; i++) {
+        	fat = 1;
+            System.out.println("Digite o valor do expoente " + (i + 1) + ": ");
+            a[i] = ler.nextInt();
+            num = a[i];
+            for (j = 1; j <= num; j++) {
+                fat *= j;
+            }
+            b[i] = fat;
+        }
+
+        System.out.print("Vetor A: ");
+        for (i = 0; i < 3; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Fatoriais: ");
+        for (i = 0; i < 3; i++) {
+            System.out.print(b[i] + " ");
+        }
+        System.out.println();
+		
+	}
+
+}
